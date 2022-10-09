e82e05c17fa62f9d8cc95e2e7f6d4bfdd446d291
Toygar Aksoy <toygar.aksoy@gmail.com>
2022-10-09 14:18:27 +0300

    app name is added to INSTALLED_APPS

--- hyde/hyde/settings.py
+++ hyde/hyde/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_extensions'
+    'django_extensions',
+    'engine'
 ]
 
 MIDDLEWARE = [

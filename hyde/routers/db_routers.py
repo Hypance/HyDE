ff038ecf8fce08e6277784e446fe1198ac934b20
Toygar Aksoy <toygar.aksoy@gmail.com>
2022-11-08 00:10:43 +0300

    DB installation is completed - will need cleanup -

--- hyde/routers/db_routers.py
+++ hyde/routers/db_routers.py
@@ -26,16 +26,16 @@ class DefaultRouter:
 
 
 class EngineRouter:
-    route_app_labels = {'engine_db'}
+    route_app_labels = {'hypance_db'}
 
     def db_for_read(self, model, **hints):
         if model._meta.app_label in self.route_app_labels:
-            return 'engine_db'
+            return 'hypance_db'
         return None
 
     def db_for_write(self, model, **hints):
         if model._meta.app_label in self.route_app_labels:
-            return 'engine_db'
+            return 'hypance_db'
         return None
 
     def allow_migrate(self, db, app_label, model_name=None, **hints):
